021bc5b2d65ca436cfa6222fbb67a5a64ac0ddbc
migbash <20924663+migbash@users.noreply.github.com>
2023-02-18 15:33:03 +0000

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { getMoralisAuth } from "@moralisweb3/client-firebase-auth-utils";
+  import { signInWithMoralis } from "@moralisweb3/client-firebase-evm-auth";
+
+  // FIXME: toggle / (comment/uncomment) these two packages below:
+  // NOTE: to remove the target `dev` error of buffer issue.
+  // import { Web3Provider } from "@ethersproject/providers";
+  // import WalletConnectProvider from "@walletconnect/web3-provider";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
